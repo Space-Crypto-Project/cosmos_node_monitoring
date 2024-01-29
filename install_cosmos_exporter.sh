697373cf547a50445cba830f05ae411054b12a8f
Space Token <83825815+Space-Crypto-Project@users.noreply.github.com>
2024-01-29 19:29:45 +0100

    Update install_cosmos_exporter.sh
    Use denom-exponent instead of coefficient

--- install_cosmos_exporter.sh
+++ install_cosmos_exporter.sh
@@ -6,8 +6,10 @@ read -p "Enter rpc_port value or hit Enter for default port [26657]: " RPC_PORT
 RPC_PORT=${RPC_PORT:-26657}
 read -p "Enter grpc_port value or hit Enter for default port [9090]: " GRPC_PORT
 GRPC_PORT=${GRPC_PORT:-9090}
-read -p "Enter chain decimal value or hit Enter for default decimal [1000000]: " CHAIN_DECIMAL
-CHAIN_DECIMAL=${CHAIN_DECIMAL:-1000000}
+#read -p "Enter chain decimal value or hit Enter for default decimal [1000000]: " CHAIN_DECIMAL
+#CHAIN_DECIMAL=${CHAIN_DECIMAL:-1000000}
+read -p "Enter the network precision or hit Enter for default precision [6]: " CHAIN_DECIMAL
+CHAIN_DECIMAL=${CHAIN_DECIMAL:-6}
 read -p "Enter account prefix or hit Enter to use the default prefix [$BENCH_PREFIX]: " ACCOUNT_PREFIX
 ACCOUNT_PREFIX=${ACCOUNT_PREFIX:-$BENCH_PREFIX}
 read -p "Enter account pubkey prefix or hit Enter to use the default prefix [$BENCH_PREFIX]: " ACCOUNT_P_PREFIX
@@ -21,8 +23,8 @@ CONS_PREFIX=${CONS_PREFIX:-$BENCH_PREFIX}
 read -p "Enter tendermint concensus pubkey prefix or hit Enter to use the default prefix [$BENCH_PREFIX]: " CONS_P_PREFIX
 CONS_P_PREFIX=${CONS_P_PREFIX:-$BENCH_PREFIX}
 
-ExecStart="cosmos-exporter --denom ${BOND_DENOM} --denom-coefficient ${CHAIN_DECIMAL} --bech-prefix ${BENCH_PREFIX} --tendermint-rpc http://localhost:${RPC_PORT} --node localhost:${GRPC_PORT}"
-
+#ExecStart="cosmos-exporter --denom ${BOND_DENOM} --denom-coefficient ${CHAIN_DECIMAL} --bech-prefix ${BENCH_PREFIX} --tendermint-rpc http://localhost:${RPC_PORT} --node localhost:${GRPC_PORT}"
+ExecStart="cosmos-exporter --denom ${BOND_DENOM} --denom-exponent ${CHAIN_DECIMAL} --bech-prefix ${BENCH_PREFIX} --tendermint-rpc http://localhost:${RPC_PORT} --node localhost:${GRPC_PORT}"
 
 if [ "$ACCOUNT_PREFIX" != "$BENCH_PREFIX" ]; then
     ExecStart="$ExecStart --bech-account-prefix $ACCOUNT_PREFIX"
